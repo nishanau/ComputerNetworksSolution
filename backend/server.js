dad7e7226614703c129a7e1caa8b8a13f044ceee
nishanau <nishanau83@gmail.com>
2024-06-13 00:36:02 +1000

    update

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,12 @@ const networkRoutes = require('./routes/networkRoutes');
 
 const app = express();
 const port = 3005;
-
-app.use(cors()); // Enable CORS
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Origin', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true
+}));
 app.use(bodyParser.json());
 app.use('/api/network', networkRoutes);
 
