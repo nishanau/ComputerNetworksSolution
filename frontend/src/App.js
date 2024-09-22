6ee25ab0b6acf894d0c014aec472fe721b0554b6
nishanau <nishanau83@gmail.com>
2024-09-22 13:52:59 +1000

    changed to http from https for simplicity

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -121,10 +121,7 @@ function App() {
 
   return (
     <Box sx={{ height: '100vh' }}>
-      <Typography variant="h5" color="red" sx={{ flexGrow: 1}}>
-            Note: Im using self signed Certificate in the server so your browser will block the request. Please allow the connection by browsing to the link
-            <Link href= "https://3.25.186.18/api/network"> here</Link> and click Proceed Anyway.. and then resubmit the inputs.
-          </Typography>
+      
       <AppBar position="static" sx={{ backgroundColor: 'transparent', boxShadow: 0 }}>
         <Toolbar>
           <Typography variant="h5" sx={{ flexGrow: 1 }}>
